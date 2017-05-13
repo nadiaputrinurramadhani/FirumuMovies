402e01c88eead85e16785a967fefcc995c24b5a3
nadiaputrinur <nadiaputri395@gmail.com>
2017-05-13 15:29:55 +0700

    Fragment

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl324/firumumovies/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl324/firumumovies/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        changePage(R.id.nav_play);
+        navigationView.setCheckedItem(R.id.nav_play);
     }
 
     @Override
@@ -77,25 +81,39 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        {
+            int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            changePage(id);
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            return true;
         }
+    }
+        // Handle navigation view item clicks here.
+
+    private void changePage(int id) {
+        Fragment fragment = null;
+
+        if (id == R.id.nav_play) {
+            fragment = new HomeFragment();
+            setTitle("Home");
+            // Handle the camera action
+        } else if (id == R.id.nav_coming) {
+            fragment = new ComingSoon();
+            setTitle("Coming Soon");
+        } else if (id == R.id.nav_popular) {
+            fragment = new Popular();
+            setTitle("Popular");
+        } else if (id == R.id.nav_about) {
+            fragment = new AboutFragment();
+            setTitle("About");
+        }
+
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment).commitNow();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
